19f47995dc86931af0aa9069fae26a1ff4898e96
sitaramarao-v-s-v-s <76678705+sitaramarao-v-s-v-s@users.noreply.github.com>
2020-12-28 13:08:10 +0530

    Update Budget_caluclation.py

--- Budget_caluclation.py
+++ Budget_caluclation.py
@@ -1,19 +1,19 @@
 print("expense calculator: ")
-while(True):
+while(True): #When ever the user enter the wrong input (anything except numbers).it will repeat the loop .
     try:
         salary=float(input("enter your salary: "))
         break
-    except ValueError:
+    except ValueError: #throw exception if salary input is not number.
         print("The salary should be a number")
         print("try again")
-total_expense=0
-expend_inventory=dict()
-balance=salary
+total_expense=0 #init base expense = 0
+expend_inventory=dict() 
+balance=salary #init balance is equal to salary
 while(True):
     try:                
-        expenditure_name=input("Enter yor expense name: ")
-        expenditure_name = expenditure_name.strip().lower()
-        if expenditure_name == 'q':
+        expenditure_name=input("Enter yor expense name : ") 
+        expenditure_name = expenditure_name.strip().lower() #remove spaces and change it to lower to ensure expenditure_names are unique
+        if expenditure_name == 'q': #exit the loop when q is entered
             break
         expenditure_amount=float(input("Enter the amount for "+ expenditure_name +" :")) 
     
@@ -21,16 +21,17 @@ while(True):
     
         print("the expenditure_amount should be a number")    
         print("try again")     
-    if(expenditure_amount <= balance):
-        balance =balance-expenditure_amount
-        if expenditure_name not in expend_inventory.keys(): 
+        
+    if(expenditure_amount <= balance): #check min balance is available or not
+        balance =balance-expenditure_amount #update balance
+        if expenditure_name not in expend_inventory.keys(): #check if key is repeated
             expend_inventory[expenditure_name]=expenditure_amount
         else: 
-            temp=expend_inventory[expenditure_name]
+            temp=expend_inventory[expenditure_name] 
             expend_inventory[expenditure_name]=temp+expenditure_amount
     else:
         print(" Insufficientfunds")
-total_expense = salary-balance
+total_expense = salary-balance #expense
 
                 
 print(expend_inventory)                
